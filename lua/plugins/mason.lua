d529c9fbd09c4c5abf1639a0ce26c61b477741b8
A1pha8977 <Alpha8977@outlook.com>
2025-10-14 14:35:40 +0900

    fix mason.lua

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -8,27 +8,30 @@ return {
     opts = {},
     config = function (_, opts)
         require("mason").setup(opts)
-        local map = require("mason-lspconfig").get_mappings().package_to_lspconfig
+        local to_lspconfig = require("mason-lspconfig").get_mappings().package_to_lspconfig
         local registry = require "mason-registry"
 
-        for _, server in ipairs(require("lsp.servers")) do
-            local found_in_registry, package = pcall(registry.get_package, server)
+        for _, server_name in ipairs(require("lsp.servers")) do
+            local found_in_registry, package = pcall(registry.get_package, server_name)
             if not found_in_registry then
-                goto check_next
+                vim.notify("mason.lua: can not find " .. server_name .. " in registry", vim.log.levels.ERROR)
+                goto check_next_server
             end
 
-            if not package.is_installed then
+            if not package:is_installed() then
+                vim.notify("mason.lua: install " .. server_name, vim.log.levels.WARN)
                 package:install()
             end
 
-            local found_config, config = pcall(require, "lsp." .. server)
+            local found_config, config = pcall(require, "lsp." .. server_name)
             if not found_config then
                 config = {}
             end
             config.capabilities = require("blink.cmp").get_lsp_capabilities()
-            vim.lsp.config(map[server], config)
-            vim.lsp.enable(map[server])
-            ::check_next::
+            vim.lsp.config(to_lspconfig[server_name], config)
+            vim.lsp.enable(to_lspconfig[server_name])
+
+            ::check_next_server::
         end
         vim.diagnostic.config({
             virtual_text = true,
